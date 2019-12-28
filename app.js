37992ec4120406b5a247d391af8cf2067e97d6fd
Matthew Yates <matt.j.p.yates@gmail.com>
2019-12-27 20:15:42 -0500

    made a lot of untested changes

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ let pubKeySchema = mongoose.Schema({
   { collection: 'public_keys' })
 let pubkeys = mongoose.model('PublicKeys', pubKeySchema)
 
+
+let tempPubKeySchema = mongoose.Schema({
+  chatid: String,
+  pubkeyhexstr: String
+},
+  { collection: 'temp_public_keys' })
+let pubkeys = mongoose.model('TempPubKeySchema', pubKeySchema)
+
+
+
 let messageSchema = mongoose.Schema({
   toid: String,
   encmessagehexstr: String,
@@ -180,6 +190,47 @@ function addMessage(tochatid, fromchatid, message, restouser) {
 
 var isTaken = false;
 
+
+function seeIfChatIDIsTakenThenRunPassedInFunct(chatidtopub, response, funct_to_pass) {
+
+  console.log("10 response is  " + typeof response);
+  mongoWrapper(function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("chatdb");
+    //var pubKeyObj = { chatid: chatidtopub, pubkeyhexstr: keystringtopub };
+    console.log("11 response is  " + typeof response);
+
+    dbo.collection("public_keys").find({ chatid: chatidtopub }, { projection: { _id: 0, chatid: 1, pubkeyhexstr: 1 } }).toArray(function (err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log("12 response is  " + typeof response);
+
+      if (result.length > 0) {
+        console.log("id taken for " + chatidtopub);
+        response.send("fail:chatidtaken")
+
+        db.close();
+      }
+      else {
+
+        console.log("13 response is  " + typeof response);
+
+        //publishPubKey(chatidtopub, keystr, response);
+        funct_to_pass();
+        db.close();
+
+      }
+    }
+
+    );
+  }
+  );
+}
+
+
+
+
 function seeIfChatIDIsTaken(chatidtopub, keystr, response) {
 
   console.log("10 response is  " + typeof response);
@@ -206,8 +257,8 @@ function seeIfChatIDIsTaken(chatidtopub, keystr, response) {
 
         console.log("13 response is  " + typeof response);
 
-        publishPubKey(chatidtopub, keystr, response);
-
+        //publishPubKey(chatidtopub, keystr, response);
+        loadKeyIntoTempKeys(chatidtopub, keystr, response);
         db.close();
 
       }
@@ -217,6 +268,39 @@ function seeIfChatIDIsTaken(chatidtopub, keystr, response) {
   }
   );
 }
+
+
+
+function loadKeyIntoTempKeys(chatidtopub, keystringtopub, restouser) {
+
+  console.log("20 response is  " + typeof restouser);
+
+
+  mongoWrapper(function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("chatdb");
+    var pubKeyObj = { chatid: chatidtopub, pubkeyhexstr: keystringtopub };
+
+
+    console.log("21 response is  " + typeof restouser);
+
+    dbo.collection("temp_public_keys").insertOne(pubKeyObj, function (err, result) {
+      if (err) {
+        throw err;
+        restouser.send("fail:database_error")
+      }
+
+      console.log("22 response is  " + typeof restouser);
+
+      setTokenForIdToBePassedIn(chatidtopub, keystringtopub, restouser);
+      //restouser.send("success");
+      db.close();
+    });
+  });
+}
+
+
+
 
 function publishPubKey(chatidtopub, keystringtopub, restouser) {
 
@@ -353,17 +437,17 @@ function lookupPubKeyForUserThenPassPubKeyToFunctionInArgs(user, funct_to_pass, 
 
 function setTokenForIdToBePassedIn(chatIdForNewToken, keyForUser, res) {
 
-  console.log("1 Here is the type of res "+ typeof res);
+  console.log("1 Here is the type of res " + typeof res);
 
-  var newTokenStr = randomstring.generate({length: 24, charset: 'alphabetic', capitalization: 'uppercase'});
+  var newTokenStr = randomstring.generate({ length: 24, charset: 'alphabetic', capitalization: 'uppercase' });
 
-//var sender = res;
+  //var sender = res;
 
-//console.log("2 Here is the type of sender "+ typeof sender);
+  //console.log("2 Here is the type of sender "+ typeof sender);
 
 
   mongoWrapper(function (err, db) {
-    console.log("3 Here is the type of sender "+ typeof res);
+    console.log("3 Here is the type of sender " + typeof res);
 
     if (err) throw err;
     var dbo = db.db("chatdb");
@@ -374,19 +458,19 @@ function setTokenForIdToBePassedIn(chatIdForNewToken, keyForUser, res) {
     child = exec("java -jar /home/ubuntu/crypto/cryptoWorker.jar -e " + keyForUser + " " + newTokenStr,
       function (error, stdout, stderr) {
 
-        console.log("4 Here is the type of sender "+ typeof res);
+        console.log("4 Here is the type of sender " + typeof res);
 
 
         dbo.collection("tokens").insertOne(newToken, function (err, result) {
           if (err) {
 
-            console.log("error on unserting the new token for "+ chatIdForNewToken);
+            console.log("error on unserting the new token for " + chatIdForNewToken);
 
             res.send("fail:database_error");
 
           }
           else {
-            console.log("5 Here is the type of sender "+ typeof res);
+            console.log("5 Here is the type of sender " + typeof res);
 
             //console.log("about to return" + "good:" + stdout);
             res.send("good:" + stdout);
@@ -462,8 +546,50 @@ function setTokenForId(chatIdForNewToken, restouser) {
 
 }
 
+function moveFromTempToPub(chatidtopub, res) {
+  mongoWrapper(function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("chatdb");
 
-function changeusername(oldname, newname) {
+    var myquery = { chatid: chatidtopub };
+
+    dbo.collection("temp_public_keys").find(myquery, { projection: { _id: 0 } }).toArray(function (err, result) {
+      if (err) throw err;
+
+      if (result.length > 0) {
+
+
+        dbo.collection("temp_public_keys").remove(myquery, true);
+
+
+        dbo.collection("public_keys").insertOne(result[0], function (err, res1) {
+          if (err) {
+
+            res.send("fail:database_error");
+
+
+            //output = "fail:database_error";
+          }
+          else {
+
+            res.send("good:keys_verified");
+
+          }
+        });
+
+
+      }
+      else {
+        response.send("fail:public_key_not_found");
+      }
+      db.close();
+    });
+
+  });
+}
+
+
+function changeusername(oldname, newname, res) {
 
   mongoWrapper(function (err, db) {
     if (err) throw err;
@@ -472,11 +598,17 @@ function changeusername(oldname, newname) {
     var myquery = { chatid: oldname };
     var newvalues = { $set: { chatid: newname } };
     dbo.collection("public_keys").updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
+      if (err) {
+        res.send("fail:database_error")
+        throw err;
+      }
     });
 
     dbo.collection("tokens").updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
+      if (err) {
+        res.send("fail:database_error");
+        throw err;
+      }
       db.close();
 
     });
@@ -526,7 +658,14 @@ app.get('/sendmessage/:tochatid/:fromchatid/:messagetosend', function (req, res)
 app.get('/publishpubkey/:chatid/:pubkeystring', function (req, res) {
   var chatidtopub = req.params.chatid;
   var keystringtopub = req.params.pubkeystring;
-  seeIfChatIDIsTaken(chatidtopub, keystringtopub, res);
+  seeIfChatIDIsTakenThenRunPassedInFunct(chatidtopub, response, function () { loadKeyIntoTempKeys(chatidtopub, keystringtopub, res); })
+  //seeIfChatIDIsTaken(chatidtopub, keystringtopub, res);
+});
+
+app.get('/verifykey/:chatid/:token', function (req, res) {
+  var chatidtopub = req.params.chatid;
+  var token = req.params.token;
+  seeIfTokenIsGoodForUserThenExecuteresponseFunction(chatidtopub, token, function () { moveFromTempToPub(chatidtopub, res) })
 });
 
 
@@ -549,7 +688,9 @@ app.get('/changechatid/:oldchatid/:newchatid/:token', function (req, res) {
   var newchatid = req.params.newchatid;
   var token = req.params.token;
 
-  seeIfTokenIsGoodForUserThenExecuteFunction(oldchatid, token,changeusername(oldchatid, newchatid), res);
+  seeIfChatIDIsTakenThenRunPassedInFunct(newchatid, res, function () {
+    seeIfTokenIsGoodForUserThenExecuteresponseFunction(oldchatid, token, changeusername(oldchatid, newchatid, res), res);
+  });
 
   //if (token == getTokenForUser(olchatid)) {
   //  changeusername(oldchatid, newchatid);
